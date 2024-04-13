ef14faf1ca5a33c73eb57b7f67d551e4f86a08a8
Anzhela Lomachevska <lomachevskaa@gmail.com>
2024-04-13 21:26:39 +0300

    Homework 5

--- hw5.js
+++ hw5.js
@@ -1,3 +1,4 @@
+//HW1
 function myisNaN(value) {
     let result;
     if (typeof value === 'number') {
@@ -9,8 +10,6 @@ function myisNaN(value) {
     console.log(result); 
     return result;
 }
-
-
 
 
 myisNaN(NaN); // true
@@ -37,3 +36,61 @@ myisNaN(new Date().toString()); // true; the string representation of a Date obj
 myisNaN([]); // false; the primitive representation is "", which coverts to the number 0
 myisNaN([1]); // false; the primitive representation is "1"
 myisNaN([1, 2]); // true; the primitive representation is "1,2", which cannot be parsed as number
+
+
+//HW2
+
+function pad(arg1, arg2, arg3, arg4) {
+    if (arg1.length >= arg3) {
+      return arg1;
+    }
+      const padSize = arg3 - arg1.length;
+      let result = '';
+    for (let i = 0; i < padSize; i++) {
+      result += arg2[i % arg2.length];
+    }
+      if (arg4) {
+      return result + arg1;
+    } else {
+      return arg1 + result;
+    }
+  }
+
+console.log(pad("abc", " ", 10, true)); // " abc"
+console.log(pad("abc", "foo", 10, true)); // "foofoofabc"
+console.log(pad("abc", "123465", 6, true)); // "123abc"
+console.log(pad("abc", "0", 8, true)); // "00000abc"
+console.log(pad("abc", " ", 1, true)); // "abc"
+
+
+//HW3
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+  function ProbabilityTheory(count) {
+    let evenCount = 0;
+    let oddCount = 0;
+    for (let i = 0; i < count; i++) {
+      const randomNumber = getRandom(100, 1000);
+      if (isEven(randomNumber)) {
+        evenCount++;
+      } else {
+        oddCount++;
+      }
+    }
+    const evenPercentage = (evenCount / count) * 100;
+    const oddPercentage = (oddCount / count) * 100;
+  
+    console.log(`Кількість згенерованих чисел: ${count}`);
+    console.log(`Парних чисел: ${evenCount}`);
+    console.log(`Непарних чисел: ${oddCount}`);
+    console.log(`Відсоток парних: ${evenPercentage.toFixed(2)}%`);
+    console.log(`Відсоток непарних: ${oddPercentage.toFixed(2)}%`);
+  }
+  
+  // Виклик функції
+  ProbabilityTheory(1000);
